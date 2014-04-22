c87690b5fe5d2db58febe4144786e644d57d4f2d
shichao <315046509@qq.com>
2014-04-22 21:39:10 +0800

    t

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,5 @@ Pxwh::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.time_zone = 'Beijing'
 end
