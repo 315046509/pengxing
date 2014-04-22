5255582d8d985e88c863f21473b1298833118161
shichao <315046509@qq.com>
2014-04-22 18:59:23 +0800

    提交

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Pxwh::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
