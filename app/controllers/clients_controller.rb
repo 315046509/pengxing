174a0d7254b2539f0550a09bfd30167205588cc7
shichao <315046509@qq.com>
2014-04-30 14:04:56 +0800

    A

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,12 @@ class ClientsController < ApplicationController
 
     @customers = Customer.order_ct_desc.page(params[:page]).per(60)
   end
+
+  def show
+    # 公司告示
+    @category_2_name = Journalism.question_category_name(2)
+    @category_2_journalism = Journalism.where({:question_category_id => 2}, "title <> ''").order_ct_desc.page(params[:page]).per(1)
+
+    @customer = Customer.find params[:id]
+  end
 end
