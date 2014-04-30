bf1df49cceef794784be03f6845594599e610a53
shichao <315046509@qq.com>
2014-04-30 13:45:44 +0800

    提交

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -10,6 +10,10 @@ class CasesController < ApplicationController
 
   # 显示页
   def show
+    # 公司告示
+    @category_2_name = Journalism.question_category_name(2)
+    @category_2_journalism = Journalism.where({:question_category_id => 2}, "title <> ''").order_ct_desc.page(params[:page]).per(1)
+
     @case = Case.find params[:id]
   end
 
