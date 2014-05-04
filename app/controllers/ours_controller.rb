dac59256af408abe17eb07b2c421726ce71f3c05
shichao <315046509@qq.com>
2014-05-04 09:59:28 +0800

    提交

--- app/controllers/ours_controller.rb
+++ app/controllers/ours_controller.rb
@@ -62,4 +62,10 @@ class OursController < ApplicationController
       redirect_to :back and return
     end
   end
+
+  def daozha
+    # 公司告示
+    @category_2_name = Journalism.question_category_name(2)
+    @category_2_journalism = Journalism.where({:question_category_id => 2}, "title <> ''").order_ct_desc.page(params[:page]).per(1)
+  end
 end
